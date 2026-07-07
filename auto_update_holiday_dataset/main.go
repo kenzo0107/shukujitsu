6fe4d4f9672321f4d4d0390551d6e6b995a450ca
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2026-07-07 11:20:12 +0900

    祝日ではない記念日・行事を除外
    - shukujitsu.yml から銀行休業日・節分・雛祭り・母の日・七夕・七五三・クリスマス・大晦日を削除
    - auto_update_holiday_dataset で今後これらが再混入しないよう除外リストを追加

--- auto_update_holiday_dataset/main.go
+++ auto_update_holiday_dataset/main.go
@@ -13,6 +13,18 @@ import (
 
 var apiKey = os.Getenv("GOOGLE_CALENDAR_API_KEY")
 
+// excludedSummaries は Google カレンダーの祝日イベントに混入する、祝日ではない記念日・行事の名称。
+var excludedSummaries = map[string]bool{
+	"銀行休業日": true,
+	"節分":    true,
+	"雛祭り":   true,
+	"母の日":   true,
+	"七夕":    true,
+	"七五三":   true,
+	"クリスマス": true,
+	"大晦日":   true,
+}
+
 func main() {
 	if err := handler(); err != nil {
 		log.Fatal(err)
@@ -40,6 +52,9 @@ func handler() error {
 
 	data := make(map[string]string, len(events.Items))
 	for _, item := range events.Items {
+		if excludedSummaries[item.Summary] {
+			continue
+		}
 		data[item.Start.Date] = item.Summary
 	}
 
